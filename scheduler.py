984313e74b7dbfa8d53a4286f66ac6c34372baff
motikumarkushwaha2-star <motikumarkushwaha2@gmail.com>
2026-07-15 09:30:55 +0300

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -3,5 +3,15 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 scheduler = AsyncIOScheduler()
 
 
+def add_daily_job(func, hour, minute):
+    scheduler.add_job(
+        func,
+        trigger="cron",
+        hour=hour,
+        minute=minute,
+        replace_existing=True
+    )
+
+
 def start_scheduler():
     scheduler.start()
